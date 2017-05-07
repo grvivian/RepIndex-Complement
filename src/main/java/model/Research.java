bba1c27b3a371a8ecc76f429473268da5aeec0de
grvivian <grvivian@gmail.com>
2017-05-07 13:04:45 -0300

    Add files via upload

--- src/main/java/model/Research.java
+++ src/main/java/model/Research.java
@@ -1,1 +1,199 @@
+package model;
 
+/**
+ *
+ * @author glaucio
+ */
+public class Research {
+
+  private int reg;
+  private String cnpq;
+  private Integer ED;
+  private Integer PA;
+  private Integer PTA;
+  private Integer MDA;
+  private Integer PEBPT;
+  private Integer PEBMD;
+  private Integer EBM;
+  private Integer RJP;
+  private Integer TPCCC;
+  private Integer TPCCM;
+  private Integer ASJ;
+  private Integer BK;
+  private Integer BC;
+  private Integer CWCP;
+  private Integer HI;
+  private Integer NC;
+  private Integer RP;
+  private Integer SOFT;
+  private Double repIndex;
+
+  public int getReg() {
+    return reg;
+  }
+
+  public void setReg(int reg) {
+    this.reg = reg;
+  }
+
+  public String getCnpq() {
+    return cnpq;
+  }
+
+  public void setCnpq(String cnpq) {
+    this.cnpq = cnpq;
+  }
+
+  public Integer getED() {
+    return ED;
+  }
+
+  public void setED(Integer ED) {
+    this.ED = ED;
+  }
+
+  public Integer getPA() {
+    return PA;
+  }
+
+  public void setPA(Integer PA) {
+    this.PA = PA;
+  }
+
+  public Integer getPTA() {
+    return PTA;
+  }
+
+  public void setPTA(Integer PTA) {
+    this.PTA = PTA;
+  }
+
+  public Integer getMDA() {
+    return MDA;
+  }
+
+  public void setMDA(Integer MDA) {
+    this.MDA = MDA;
+  }
+
+  public Integer getPEBPT() {
+    return PEBPT;
+  }
+
+  public void setPEBPT(Integer PEBPT) {
+    this.PEBPT = PEBPT;
+  }
+
+  public Integer getPEBMD() {
+    return PEBMD;
+  }
+
+  public void setPEBMD(Integer PEBMD) {
+    this.PEBMD = PEBMD;
+  }
+
+  public Integer getEBM() {
+    return EBM;
+  }
+
+  public void setEBM(Integer EBM) {
+    this.EBM = EBM;
+  }
+
+  public Integer getRJP() {
+    return RJP;
+  }
+
+  public void setRJP(Integer RJP) {
+    this.RJP = RJP;
+  }
+
+  public Integer getTPCCC() {
+    return TPCCC;
+  }
+
+  public void setTPCCC(Integer TPCCC) {
+    this.TPCCC = TPCCC;
+  }
+
+  public Integer getTPCCM() {
+    return TPCCM;
+  }
+
+  public void setTPCCM(Integer TPCCM) {
+    this.TPCCM = TPCCM;
+  }
+
+  public Integer getASJ() {
+    return ASJ;
+  }
+
+  public void setASJ(Integer ASJ) {
+    this.ASJ = ASJ;
+  }
+
+  public Integer getBK() {
+    return BK;
+  }
+
+  public void setBK(Integer BK) {
+    this.BK = BK;
+  }
+
+  public Integer getBC() {
+    return BC;
+  }
+
+  public void setBC(Integer BC) {
+    this.BC = BC;
+  }
+
+  public Integer getCWCP() {
+    return CWCP;
+  }
+
+  public void setCWCP(Integer CWCP) {
+    this.CWCP = CWCP;
+  }
+
+  public Integer getHI() {
+    return HI;
+  }
+
+  public void setHI(Integer HI) {
+    this.HI = HI;
+  }
+
+  public Integer getNC() {
+    return NC;
+  }
+
+  public void setNC(Integer NC) {
+    this.NC = NC;
+  }
+
+  public Integer getRP() {
+    return RP;
+  }
+
+  public void setRP(Integer RP) {
+    this.RP = RP;
+  }
+
+  public Integer getSOFT() {
+    return SOFT;
+  }
+
+  public void setSOFT(Integer SOFT) {
+    this.SOFT = SOFT;
+  }
+
+  public Double getRepIndex() {
+    return repIndex;
+  }
+
+  public void setRepIndex(Double repIndex) {
+    this.repIndex = repIndex;
+  }
+
+}
