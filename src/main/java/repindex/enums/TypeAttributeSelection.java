86db82ac470136c5a54c235ef3cfd7e654a0ea6d
grvivian <grvivian@gmail.com>
2017-05-07 13:11:23 -0300

    Add files via upload

--- src/main/java/repindex/enums/TypeAttributeSelection.java
+++ src/main/java/repindex/enums/TypeAttributeSelection.java
@@ -1,1 +1,9 @@
+package repindex.enums;
 
+/**
+ *
+ * @author glaucio
+ */
+public enum TypeAttributeSelection {
+  REP_INDEX_ORIGINAL, GAIN_RATIO, INFO_GAIN, CHI_SQUARED, RELIEF_F, SYMMETRICAL_UNCERT/*, SVM*/;
+}
